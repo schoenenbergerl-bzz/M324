c0e6b57f226f0b34acc6f083daddf58dc26681d6
lucas <schoenenbergerl@bzz.ch>
2024-11-27 14:07:48 +0100

    first commit for M324

---
+++ trafficlight.test.js
@@ -0,0 +1,13 @@
+const app = require("./trafficlight");
+
+describe('test suite traffic light', () => {
+    it(`case red light`, () => {
+        expect(app.trafficLight("red")).toBe("DON'T WALK!");
+    });
+});
+
+describe('test suite traffic light', () => {
+    it(`case red light`, () => {
+        expect(app.trafficLight("red")).toBe("DON'T WALK!");
+    });
+});
